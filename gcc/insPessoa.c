2ebdacb9ce9c89703788f7549cf130b99e27cf64
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2022-01-31 16:16:18 -0300

    Add files via upload

--- gcc/insPessoa.c
+++ gcc/insPessoa.c
@@ -6,16 +6,17 @@
 #define USER "root"
 #define HOST "127.0.0.1"
 #define DATABASE "testedb"
-#define PASS "226468"
+
 
 #define SUCCESS 0
 #define ERROR 1
 
-MYSQL mycon;
+MYSQL *mycon;
+char PASS[20];
 int res;
 
 int conecta(void){
-	mysql_init(&mycon);
+	mycon = mysql_init(NULL);
 	/*
 	mysql_real_connect(MYSQL *mysql,
                    const char *host,
@@ -27,7 +28,7 @@ int conecta(void){
                    unsigned long client_flag)
 				  */
 	if((res = mysql_real_connect(
-				  &mycon, 
+				  mycon, 
 				  HOST, 
 				  USER,
 				  PASS,
@@ -57,20 +58,22 @@ typedef struct PESSOAS {
 
 
 void desconecta(void){
-   mysql_close(&mycon);
+   mysql_close(mycon);
    printf("Desconectado\n");
 }
 
 int insere( PESSOAS *pes){
   char sql[500];
   memset(sql,'\0',sizeof(sql));
-  sprintf(sql,"insert into pessoas (nome, dtnasc, profissao, sexo) values ('%s','%s','%s','%c');", 
+  sprintf(sql,"insert into pessoas (nome, dtnasc, profissao, sexo) values ('%s','%s','%s','%c')", 
        pes->nome,
 	   pes->dtnasc,
 	   pes->profissao,
 	   pes->sexo);
-  res = mysql_query(&mycon,sql);
-  if(res=SUCCESS) {
+  printf("SQL:%s\n\n",sql);	   
+  //mysql_prepare(
+  res = mysql_real_query(mycon,sql,sizeof(sql));
+  if(!res) {
 	  printf("Registrado com sucesso!\n");
   }  else {
 	  printf("Falha na inclusão\n");
@@ -80,14 +83,15 @@ int insere( PESSOAS *pes){
 void captura_dados(PESSOAS *pes){
 	printf("Digite as informações que deseja registrar\n");
 	printf("==========================================\n");
-	printf("\nNome:");
+	printf("\nNome:"); 
 	scanf("%s",pes->nome);
-	printf("\nDt Nascimento:");
+	printf("\nDt Nascimento(yyyy-mm-dd):");
 	scanf("%s",pes->dtnasc);
 	printf("\nProfissao:");
 	scanf("%s",pes->profissao);
 	printf("\nSexo(M/F):");
 	scanf("%c",&pes->sexo);	
+	if((pes->sexo!='M') || (pes->sexo!='F')) { pes->sexo = 'M'; } /*Valor default*/
 	printf("\n\n");
 }
 
@@ -95,6 +99,8 @@ void Wellcome(void){
    printf("Software insPessoa\n");
    printf("Criado por Marcelo Maurin Martins\n");
    printf("Maurinsoft.com.br\n");
+   printf("Senha do banco:");
+   scanf("%s",PASS);
 }
 
 //funcao principal
