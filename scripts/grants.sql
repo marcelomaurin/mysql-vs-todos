1c75fd69fc24014ac4c366152f12e3ffe2ee2043
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2022-02-04 10:44:36 -0300

    Add files via upload

--- scripts/grants.sql
+++ scripts/grants.sql
@@ -1,5 +1,6 @@
 CREATE USER IF NOT EXISTS  'gcc'@'localhost' IDENTIFIED BY '123456';
 
 GRANT ALL ON *.* TO 'gcc'@'localhost' WITH GRANT OPTION;
+GRANT ALL ON *.* to 'root'@'localhost' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
