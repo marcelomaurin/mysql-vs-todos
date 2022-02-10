63d93e09d6a11a04e2cfaab14004aea6053860df
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2022-02-10 14:15:15 -0300

    Add files via upload

---
+++ php/config.php
@@ -0,0 +1,15 @@
+﻿<?php
+    //Inclua aqui as configurações do seu banco de dados
+	$dbhost        = '127.0.0.1';
+	$database      = 'testedb';
+	$dbuser        = 'php';
+	$dbpassword    = '123456';
+	$urlsrv        = 'maurinsoft.com.br';
+	$URLLABEL      = 'Casa Inteligente';	
+	$emailreply    = 'marcelomaurinmartins@gmail.com';
+	$titlesite     = 'Projeto Casa Inteligente';
+	$erro_redirect = 'construcao.php';
+	$erro_access = 'notaccess.php';
+	$homename      = 'Principal';
+
+?>
